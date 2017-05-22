e4770a5dbeee4adf6293083fedd783a3066c516c
Emi92ar <emibentive@hotmail.com>
2017-05-22 12:15:59 -0300

    doesn't work spring

--- src/main/java/persistence/DataBaseDAO.java
+++ src/main/java/persistence/DataBaseDAO.java
@@ -2,13 +2,15 @@ package persistence;
 
 import java.sql.Connection;
 
+import org.springframework.stereotype.Component;
+
 import model.Actualday;
 import model.Day;
 import model.Actualday.Astronomy;
 import model.Actualday.Atmosphere;
 import model.Actualday.Location;
 import model.Actualday.Wind;
-
+@Component
 public interface DataBaseDAO {
 	
 	public Connection getCon();

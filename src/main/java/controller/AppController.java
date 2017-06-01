e668f828c33e1f0cc0545d74b90874624f1170a8
Emi92ar <emibentive@hotmail.com>
2017-06-01 19:48:59 -0300

    trying to work with rest

---
+++ src/main/java/controller/AppController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/*path variable no va con get
+* requestmapping va con get, put y post, va el string path
+* queryParam permite conjunto de parametros con signo de pregunta, nombre de la variable
+* y al lado el valor, para concatenar pongo un & y agrego lo que necesite
+* queryparam siempre va con get
+*/
+@RestController
+public class AppController {
+
+	
+	@RequestMapping("/")
+    public String bienvenida () {
+        return "HOME PAGE by Emi";
+    }
+	
+	@RequestMapping(value = "/location/{namelocation}", method=RequestMethod.GET)
+    public String getweather (@PathVariable("namelocation") String namelocation)  {
+        return "funciona el get";
+    }
+}
